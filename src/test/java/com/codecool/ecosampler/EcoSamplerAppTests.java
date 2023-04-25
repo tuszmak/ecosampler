c540907dcca4ce355fa3707d0f21ffd7568cf79f
MateWagner <wwagnermate@gmail.com>
2023-04-25 10:44:14 +0200

    Init Project

---
+++ src/test/java/com/codecool/ecosampler/EcoSamplerAppTests.java
@@ -0,0 +1,13 @@
+package com.codecool.ecosampler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSamplerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
