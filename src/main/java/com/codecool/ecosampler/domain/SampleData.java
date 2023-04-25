677c9a29c8c9b6956d0e27854e3ef14bb2a6fe30
MateWagner <wwagnermate@gmail.com>
2023-04-25 13:05:44 +0200

    Add initial SampleData Entity

---
+++ src/main/java/com/codecool/ecosampler/domain/SampleData.java
@@ -0,0 +1,30 @@
+package com.codecool.ecosampler.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "sample_data")
+public class SampleData {
+    @Id
+    @SequenceGenerator(
+            name = "user_id_sequens",
+            sequenceName = "user_id_sequens",
+            allocationSize = 1
+
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "user_id_sequens")
+    private Long id;
+
+    @OneToOne(targetEntity = User.class)
+    private User user;
+
+    @OneToOne(targetEntity = Form.class)
+    private Form form;
+}
