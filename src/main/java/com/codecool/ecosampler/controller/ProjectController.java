bb7aa718b76672cd4c9d5ec3ace929a1cba2b7c1
MateWagner <wwagnermate@gmail.com>
2023-04-26 13:18:26 +0200

    feat: ProjectController initial implementation

---
+++ src/main/java/com/codecool/ecosampler/controller/ProjectController.java
@@ -0,0 +1,42 @@
+package com.codecool.ecosampler.controller;
+
+import com.codecool.ecosampler.domain.Project;
+import com.codecool.ecosampler.service.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/project")
+public class ProjectController {
+    private ProjectService projectService;
+
+    @GetMapping
+    public List<Project> getAllProject() {
+        return projectService.getAllProject();
+    }
+
+    @PutMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Project addNewProject(@RequestBody Project project) {
+        return projectService.addNewProject(project);
+    }
+
+    @GetMapping("/by-user/{userId}")
+    public List<Project> getProjectsByUserId(@PathVariable Long userId) {
+        return projectService.getProjectsByUserId(userId);
+    }
+
+    @DeleteMapping("{projectId}")
+    public void deleteProject(@PathVariable Long projectId) {
+        projectService.deleteProject(projectId);
+    }
+
+    @PostMapping("{projectId}")
+    public Project updateProject(@PathVariable Long projectId, @RequestBody Project project) {
+        return projectService.updateProject(projectId, project);
+    }
+}
