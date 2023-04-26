8592f62f17dfc43dcede61b8659f006903b9cce0
tuszmak <daniel.pinter02@gmail.com>
2023-04-26 11:23:13 +0200

    style: Repositories has annotations

--- src/main/java/com/codecool/ecosampler/repository/FormRepository.java
+++ src/main/java/com/codecool/ecosampler/repository/FormRepository.java
@@ -2,7 +2,9 @@ package com.codecool.ecosampler.repository;
 
 import com.codecool.ecosampler.domain.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FormRepository extends JpaRepository<Form, Long> {
 
 }
--- src/main/java/com/codecool/ecosampler/repository/UserRepository.java
+++ src/main/java/com/codecool/ecosampler/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.codecool.ecosampler.repository;
 
 import com.codecool.ecosampler.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByEmail(String email);
 
