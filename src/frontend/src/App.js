69deb43cf4d8abfb08069655cb37e27acfb25d6f
MateWagner <wwagnermate@gmail.com>
2023-04-26 15:47:33 +0200

    feat: react router initial config antd test button

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Sample from './Sample';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Project Here</h1>
+      <Button type="primary">Button</Button>
+      <Routes>
+        <Route path='/' element={<Sample />} />
+      </Routes>
     </div>
   );
 }
