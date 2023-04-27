70e808c25001675c6e553ca83e0f093966c6e4e9
MateWagner <wwagnermate@gmail.com>
2023-04-27 11:54:31 +0200

    fix: remove console log

--- src/frontend/src/pages/projects/ProjectList.js
+++ src/frontend/src/pages/projects/ProjectList.js
@@ -1,10 +1,7 @@
 import { Table } from "antd";
 import { Link } from "react-router-dom";
 
-
 const ProjectList = ({ projects }) => {
-  console.log(projects);
-
   const columns = [
     {
       title: 'Id',
@@ -18,6 +15,7 @@ const ProjectList = ({ projects }) => {
       render: (text, record) => <Link to={`/project/${record.id}`}>{text}</Link>,
     }
   ];
+
   return (
     <div>
       <h1>Projects</h1>
