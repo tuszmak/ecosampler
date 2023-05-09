686e05964c0c7753b50acd4952229926798ab396
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-09 15:00:07 +0200

    feat: test is faster

--- src/test/java/com/codecool/ecosampler/service/AnswerServiceTest.java
+++ src/test/java/com/codecool/ecosampler/service/AnswerServiceTest.java
@@ -1,12 +1,9 @@
 package com.codecool.ecosampler.service;
 
-import com.codecool.ecosampler.controller.dto.answer.AnswerDTO;
-import com.codecool.ecosampler.controller.dto.answer.NewAnswer;
 import com.codecool.ecosampler.domain.Answer;
 import com.codecool.ecosampler.domain.Question;
 import com.codecool.ecosampler.repository.AnswerRepository;
 import com.codecool.ecosampler.utilities.AnswerMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -18,17 +15,20 @@ import static org.mockito.Mockito.*;
 
 class AnswerServiceTest {
 
-    private AnswerRepository answerRepository;
-    private AnswerService answerService;
+    private final AnswerRepository answerRepository;
+    private final AnswerService answerService;
     private QuestionService questionService;
 
-
-    @BeforeEach
-    public void setup() {
-        answerRepository = Mockito.mock(AnswerRepository.class);
-        answerService = new AnswerService(answerRepository, new AnswerMapper(), questionService);
-
+    public AnswerServiceTest() {
+        this.answerRepository = Mockito.mock(AnswerRepository.class);
+        this.answerService = new AnswerService(answerRepository, new AnswerMapper(), questionService);
     }
+
+//    @BeforeEach
+//    public void setup() {
+//
+//
+//    }
 
 
     @Test
@@ -37,7 +37,7 @@ class AnswerServiceTest {
         when(answerRepository.findAll()).thenReturn(List.of(answer));
         int numberOfAnswers = answerService.getAllAnswersDTO().size();
         assertEquals(1, numberOfAnswers);
-        verify(answerRepository,times(1)).findAll();
+        verify(answerRepository, times(1)).findAll();
     }
 
     @Test
