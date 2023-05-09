2913cafb25fc6dee74ca1b5272bae28f29c8bbbc
MateWagner <wwagnermate@gmail.com>
2023-05-09 11:35:07 +0200

    fix: Form: When New Entity created return a DTO

--- src/main/java/com/codecool/ecosampler/service/FormService.java
+++ src/main/java/com/codecool/ecosampler/service/FormService.java
@@ -1,9 +1,11 @@
 package com.codecool.ecosampler.service;
 
+import com.codecool.ecosampler.controller.dto.form.FormDTO;
 import com.codecool.ecosampler.controller.dto.form.NewForm;
 import com.codecool.ecosampler.domain.Form;
 import com.codecool.ecosampler.exeption.NotFoundException;
 import com.codecool.ecosampler.repository.FormRepository;
+import com.codecool.ecosampler.utilities.FormMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +14,16 @@ import java.util.UUID;
 @AllArgsConstructor
 @Service
 public class FormService {
-    private FormRepository formRepository;
+    private final FormRepository formRepository;
+    private final FormMapper formMapper;
 
-    public UUID createNewForm(NewForm form) {
-        return formRepository.save(new Form(
-                                UUID.randomUUID(),
-                                form.name()
-                        )
+    public FormDTO createNewForm(NewForm newForm) {
+        final Form form = formRepository.save(new Form(
+                        UUID.randomUUID(),
+                        newForm.name()
                 )
-                .getPublicId();
+        );
+        return formMapper.toDTO(form);
     }
 
     public Form getFormByPublicId(UUID publicId) {
