7f4ee388e525917236c61b4d41d421f837d21d67
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-09 13:25:39 +0200

    feat: make answer-sampledata connection

--- src/main/java/com/codecool/ecosampler/utilities/LineRunner.java
+++ src/main/java/com/codecool/ecosampler/utilities/LineRunner.java
@@ -1,24 +1,24 @@
 package com.codecool.ecosampler.utilities;
 
 import com.codecool.ecosampler.domain.*;
-import com.codecool.ecosampler.repository.FormRepository;
-import com.codecool.ecosampler.repository.ProjectRepository;
-import com.codecool.ecosampler.repository.QuestionRepository;
-import com.codecool.ecosampler.repository.UserRepository;
+import com.codecool.ecosampler.repository.*;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Component
 @AllArgsConstructor
 public class LineRunner implements CommandLineRunner {
+    private AnswerRepository answerRepository;
     private UserRepository userRepository;
     private ProjectRepository projectRepository;
     private FormRepository formRepository;
     private QuestionRepository questionRepository;
+    private SampleDataRepository sampleDataRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,14 +33,19 @@ public class LineRunner implements CommandLineRunner {
         Form form1 = new Form(UUID.randomUUID(), "Appearance");
         Form form2 = new Form(UUID.randomUUID(), "Size");
         Question question1 = new Question(UUID.randomUUID(), "What color is it?", FieldStyle.SHORT_TEXT);
-        form1.addQuestion(question1);
+        Answer answer = new Answer(UUID.randomUUID(), "Brown", question1);
+        SampleData sampleData = new SampleData(UUID.randomUUID(), LocalDateTime.now(), user3, form1);
 
         project1.addUserToProject(user2);
         project1.addFormToProject(form2);
+        form1.addQuestion(question1);
+        answer.setSampleData(sampleData);
         userRepository.saveAll(List.of(user1, user2, user3));
         questionRepository.save(question1);
         formRepository.saveAll(List.of(form1, form2));
         projectRepository.save(project1);
+        sampleDataRepository.save(sampleData);
+        answerRepository.save(answer);
 
     }
 }
