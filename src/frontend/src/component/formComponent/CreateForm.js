ef4c30f5b3d5c074596621357fd275897fc879c7
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-10 13:51:50 +0200

    feat: allow to post a form to backend

--- src/frontend/src/component/formComponent/CreateForm.js
+++ src/frontend/src/component/formComponent/CreateForm.js
@@ -3,14 +3,22 @@ import React from "react";
 import { Button, Form, Input, Radio } from "antd";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
-const onFinish = (values) => {
-  console.log('Success:', values);
+const onFinish = async (values) => {
+  const response = await fetch("/api/v1/form/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(values),
+  });
+//   console.log("Success:", values);
+console.log(response.status);
 };
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+  console.log("Failed:", errorInfo);
 };
 export const CreateForm = () => {
-    console.log(useLocation().pathname.split("create-form/")[1]);
+  console.log(useLocation().pathname.split("create-form/")[1]);
   return (
     <Form
       name="basic"
@@ -30,21 +38,9 @@ export const CreateForm = () => {
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-      <Form.Item
-        label="Form name"
-        name="formname"
-        required="true"
-      >
+      <Form.Item label="Form name" name="name" required="true">
         <Input />
       </Form.Item>
-
-      <Form.Item
-        label="Type"
-        name="type"
-      >
-        <Input />
-      </Form.Item>
-
       <Form.Item
         wrapperCol={{
           offset: 8,
