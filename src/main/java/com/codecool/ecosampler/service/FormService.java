d0336f533bc0d6ab067ae7dd7bd96ab188fabdb7
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-11 11:10:57 +0200

    feat: make endpoint to get questions from backend

--- src/main/java/com/codecool/ecosampler/service/FormService.java
+++ src/main/java/com/codecool/ecosampler/service/FormService.java
@@ -2,10 +2,12 @@ package com.codecool.ecosampler.service;
 
 import com.codecool.ecosampler.controller.dto.form.FormDTO;
 import com.codecool.ecosampler.controller.dto.form.NewForm;
+import com.codecool.ecosampler.controller.dto.question.QuestionDTO;
 import com.codecool.ecosampler.domain.Form;
 import com.codecool.ecosampler.exeption.NotFoundException;
 import com.codecool.ecosampler.repository.FormRepository;
 import com.codecool.ecosampler.utilities.FormMapper;
+import com.codecool.ecosampler.utilities.QuestionMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,7 @@ import java.util.stream.Collectors;
 public class FormService {
     private final FormRepository formRepository;
     private final FormMapper formMapper;
+    private final QuestionMapper questionMapper;
 
     public FormDTO createNewFormGetDTO(NewForm newForm) {
         final Form form = formRepository.save(new Form(UUID.randomUUID(), newForm.name()));
@@ -35,5 +38,13 @@ public class FormService {
     public List<FormDTO> getFormsByProjectID(UUID projectID) {
         return formRepository.findFormsByProjectID(projectID).stream().map(formMapper::toDTO).collect(Collectors.toList());
 
+    }
+
+    public List<QuestionDTO> getQuestionDTOsByFormID(UUID formID) {
+        Form form = formRepository.findFormByPublicId(formID)
+                .orElseThrow(() -> new RuntimeException("No form found"));
+        return form.getQuestions().stream()
+                .map(questionMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
