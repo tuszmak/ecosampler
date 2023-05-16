248d85358c61c42d2793224e9768b774fc988f81
MateWagner <wwagnermate@gmail.com>
2023-05-16 23:01:13 +0200

    feat: dto with two list to modify users on project

--- src/main/java/com/codecool/ecosampler/controller/dto/project/ModifyUsersOnProject.java
+++ src/main/java/com/codecool/ecosampler/controller/dto/project/ModifyUsersOnProject.java
@@ -3,5 +3,5 @@ package com.codecool.ecosampler.controller.dto.project;
 import java.util.List;
 import java.util.UUID;
 
-public record ProjectModifyUsers(UUID projectID, List<UUID> addUserIDs, List<UUID> removeUserIDs) {
+public record ModifyUsersOnProject(List<UUID> addUserIDs, List<UUID> removeUserIDs) {
 }
