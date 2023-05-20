a3f7decb104fd1db4852ec759842e894dab1b964
MateWagner <wwagnermate@gmail.com>
2023-05-20 16:35:43 +0200

    feat: JWT creation validation related methods

---
+++ src/main/java/com/codecool/ecosampler/security/JWTService.java
@@ -0,0 +1,74 @@
+package com.codecool.ecosampler.security;
+
+import com.codecool.ecosampler.domain.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+
+    public String generateToken(User user) {
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        String token = Jwts.builder()
+                .setClaims(generateExtraClaims(user))
+                .setSubject(user.getEmail())
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(getSigInKey())
+                .compact();
+        return token;
+    }
+
+    private Map<String, Object> generateExtraClaims(User user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("userID", user.getPublicId());
+        extraClaims.put("role", user.getRole());
+        extraClaims.put("name", user.getName());
+        return extraClaims;
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigInKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect");
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SecurityConstants.JWT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
