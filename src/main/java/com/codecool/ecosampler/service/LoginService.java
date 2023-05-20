54aa7f702b5df67cc36ecf55b44c5f3dc6d3917f
MateWagner <wwagnermate@gmail.com>
2023-05-20 17:17:46 +0200

    feat: Implement login with return jwt token

--- src/main/java/com/codecool/ecosampler/service/LoginService.java
+++ src/main/java/com/codecool/ecosampler/service/LoginService.java
@@ -1,26 +1,33 @@
 package com.codecool.ecosampler.service;
 
-import com.codecool.ecosampler.controller.dto.login.LoginCredentials;
+import com.codecool.ecosampler.controller.dto.TokenDTO;
 import com.codecool.ecosampler.controller.dto.login.LoginRequest;
 import com.codecool.ecosampler.domain.User;
-import com.codecool.ecosampler.repository.UserRepository;
-import com.codecool.ecosampler.utilities.UserMapper;
+import com.codecool.ecosampler.security.JWTService;
+import com.codecool.ecosampler.utilities.TokenMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @AllArgsConstructor
 @Service
 public class LoginService {
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
+    private final UserService userService;
 
-    private final UserRepository userRepository;
-    public LoginCredentials verifyUser(LoginRequest loginRequest) {
-       return UserMapper.mapToResponseUser(getUserByLogin(loginRequest));
+    public TokenDTO verifyUser(LoginRequest loginRequest) {
+       return getJWTokenAndAuthenticateUser(loginRequest);
     }
 
-    private User getUserByLogin(LoginRequest loginRequest) {
-        return userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong email or password!"));
+    private TokenDTO getJWTokenAndAuthenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        User user = userService.getUserByEmail(loginRequest.email());
+        String token = jwtService.generateToken(user);
+        return TokenMapper.toTokenDTO(token);
     }
 }
