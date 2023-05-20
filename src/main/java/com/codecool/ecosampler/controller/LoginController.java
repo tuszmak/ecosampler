b35dc67c73d853947281385f65a5f7e355fae154
MateWagner <wwagnermate@gmail.com>
2023-05-20 17:12:37 +0200

    feat: verify user return TokenDTO

--- src/main/java/com/codecool/ecosampler/controller/LoginController.java
+++ src/main/java/com/codecool/ecosampler/controller/LoginController.java
@@ -1,21 +1,23 @@
 package com.codecool.ecosampler.controller;
 
 
+import com.codecool.ecosampler.controller.dto.TokenDTO;
 import com.codecool.ecosampler.controller.dto.login.LoginRequest;
-import com.codecool.ecosampler.controller.dto.login.LoginCredentials;
 import com.codecool.ecosampler.service.LoginService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/login")
 public class LoginController {
-
-    private LoginService loginService;
+    private final LoginService loginService;
 
     @PostMapping
-    public LoginCredentials verifyUser(@RequestBody LoginRequest loginRequest) {
+    public TokenDTO verifyUser(@RequestBody LoginRequest loginRequest) {
         return loginService.verifyUser(loginRequest);
     }
 }
