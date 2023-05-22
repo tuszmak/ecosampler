5286f588e335ac2bf12e4e6e7050adb5284c1b12
MateWagner <wwagnermate@gmail.com>
2023-05-22 10:19:36 +0200

    remove dto and mapper due changed concept

--- src/main/java/com/codecool/ecosampler/utilities/TokenMapper.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.ecosampler.utilities;
-
-import com.codecool.ecosampler.controller.dto.TokenDTO;
-
-public class TokenMapper {
-    public static TokenDTO toTokenDTO(String token) {
-        return new TokenDTO(token);
-    }
-}
