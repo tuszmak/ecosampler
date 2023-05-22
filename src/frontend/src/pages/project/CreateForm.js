3841a74f77426fda2f5eb4484f8bc95da55ea7d9
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-22 12:58:17 +0200

    feat: implement form creation with questions

--- src/frontend/src/pages/project/CreateForm.js
+++ src/frontend/src/pages/project/CreateForm.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 
-import { Button, Form, Input, Radio, message } from "antd";
+import { Button, Form, Input, Radio, Select, Space, message } from "antd";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 
-const path = "/api/v1/project/addForm";
+const path = "/api/v1/project/addForm/";
 export const CreateForm = () => {
-  const [params, setparams] = useState(useParams());
+  const [params, setParams] = useState(useParams());
+  const [fieldType, setFieldType] = useState("SHORT-TEXT");
   const navigate = useNavigate();
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+    setFieldType(value);
+  };
   const onFinish = async (values) => {
+    console.log(values);
     try {
       await fetch(path + params.id, {
         method: "POST",
@@ -27,31 +34,94 @@ export const CreateForm = () => {
 
   return (
     <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
+      name="dynamic_form_nest_item"
+      onFinish={onFinish}
       style={{
         maxWidth: 600,
       }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
       autoComplete="off"
     >
       <Form.Item label="Form name" name="name" required="true">
         <Input />
       </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
+      <Form.List name="questions">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map(({ key, name, ...restField }) => (
+              <Space
+                key={key}
+                style={{
+                  display: "flex",
+                  marginBottom: 8,
+                }}
+                align="baseline"
+              >
+                <Form.Item
+                  {...restField}
+                  name={[name, "description"]}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Missing question",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Question" />
+                </Form.Item>
+                <Form.Item
+                  {...restField}
+                  name={[name, "fieldStyle"]}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Missing field type",
+                    },
+                  ]}
+                >
+                  <Select
+                    value={fieldType}
+                    style={{
+                      width: 120,
+                    }}
+                    onChange={handleChange}
+                    options={[
+                      {
+                        value: "SHORT_TEXT",
+                        label: "Short text",
+                      },
+                      {
+                        value: "lucy",
+                        label: "Lucy",
+                      },
+                      {
+                        value: "Yiminghe",
+                        label: "yiminghe",
+                      },
+                      {
+                        value: "disabled",
+                        label: "Disabled",
+                        disabled: true,
+                      },
+                    ]}
+                  />
+                </Form.Item>
+                <MinusCircleOutlined onClick={() => remove(name)} />
+              </Space>
+            ))}
+            <Form.Item>
+              <Button
+                type="dashed"
+                onClick={() => add()}
+                block
+                icon={<PlusOutlined />}
+              >
+                Add field
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
