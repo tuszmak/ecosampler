c48574b55a09d08949dfc2b2eb8fca124aa5bda0
gerigol <golengerhard@gmail.com>
2023-05-25 13:21:48 +0200

    feat: secure endpoints by role

--- src/main/java/com/codecool/ecosampler/security/SecurityConfig.java
+++ src/main/java/com/codecool/ecosampler/security/SecurityConfig.java
@@ -14,11 +14,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.codecool.ecosampler.domain.Role.DIRECTOR;
+import static com.codecool.ecosampler.domain.Role.PROJECT_LEADER;
+import static org.springframework.http.HttpMethod.*;
+
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -31,7 +36,19 @@ public class SecurityConfig {
                         SecuritySessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/login").permitAll()
+                        .requestMatchers("/api/v1/login")
+                        .permitAll()
+
+                        .requestMatchers(POST, "/api/v1/project").hasRole(DIRECTOR.name())
+                        .requestMatchers(POST, "/api/v1/project/addForm/{projectID}").hasAnyRole(DIRECTOR.name(), PROJECT_LEADER.name())
+                        .requestMatchers(DELETE, "/api/v1/project/{publicProjectId}").hasRole(DIRECTOR.name())
+                        .requestMatchers(PUT, "/api/v1/project/modify-users/{projectID}").hasAnyRole(DIRECTOR.name())
+                        .requestMatchers(PUT, "/api/v1/question/{publicId}").hasAnyRole(DIRECTOR.name(), PROJECT_LEADER.name())
+                        .requestMatchers(DELETE, "/api/v1/question/{publicId}").hasAnyRole(DIRECTOR.name(), PROJECT_LEADER.name())
+                        .requestMatchers(POST, "/api/v1/form/**").hasAnyRole(DIRECTOR.name(), PROJECT_LEADER.name())
+                        .requestMatchers(POST, "/api/v1/user").hasRole(DIRECTOR.name())
+                        .requestMatchers(POST, "/api/v1/user/{publicId}").hasRole(DIRECTOR.name())
+
                         .anyRequest().authenticated()
                 );
         return http.build();
