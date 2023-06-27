ba32a9c9de58381e749f4a9200b232fbc3429ed7
gerigol <golengerhard@gmail.com>
2023-06-27 15:24:13 +0200

    refactor:  NewUser UUID generated by UserMapper

--- src/main/java/com/codecool/ecosampler/controller/dto/user/NewUser.java
+++ src/main/java/com/codecool/ecosampler/controller/dto/user/NewUser.java
@@ -2,7 +2,5 @@ package com.codecool.ecosampler.controller.dto.user;
 
 import com.codecool.ecosampler.domain.Role;
 
-import java.util.UUID;
-
-public record NewUser(UUID public_id, String name, String email, Role role, String password) {
+public record NewUser(String name, String email, Role role, String password) {
 }
